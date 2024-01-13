b9c95eb2ce3b428679a758c9b506a400729f160b
Moleculer <hello@moleculer.services>
2024-01-13 14:05:37 +0000

    Generated files

---
+++ services/greeter.service.ts
@@ -0,0 +1,87 @@
+import type { Context, Service, ServiceSchema, ServiceSettingSchema } from "moleculer";
+
+export interface ActionHelloParams {
+	name: string;
+}
+
+interface GreeterSettings extends ServiceSettingSchema {
+	defaultName: string;
+}
+
+interface GreeterMethods {
+	uppercase(str: string): string;
+}
+
+interface GreeterLocalVars {
+	myVar: string;
+}
+
+type GreeterThis = Service<GreeterSettings> & GreeterMethods & GreeterLocalVars;
+
+const GreeterService: ServiceSchema<GreeterSettings> = {
+	name: "greeter",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+		defaultName: "Moleculer",
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		hello: {
+			rest: {
+				method: "GET",
+				path: "/hello",
+			},
+			handler(this: GreeterThis/* , ctx: Context */): string {
+				return `Hello ${this.settings.defaultName}`;
+			},
+		},
+
+		welcome: {
+			rest: "GET /welcome/:name",
+			params: {
+				name: "string",
+			},
+			handler(this: GreeterThis, ctx: Context<ActionHelloParams>): string {
+				return `Welcome, ${ctx.params.name}`;
+			},
+		},
+	},
+
+	/**
+	 * Events
+	 */
+	events: {},
+
+	/**
+	 * Methods
+	 */
+	methods: {},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {},
+};
+
+export default GreeterService;
