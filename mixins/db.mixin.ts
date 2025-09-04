a84f0051a8b772baf4efd9c85894f6f445cc0157
Moleculer <hello@moleculer.services>
2025-09-04 20:52:48 +0000

    Generated files (fbc0b8cf7d29ffbc1dddaec243106c853daeb082)

---
+++ mixins/db.mixin.ts
@@ -0,0 +1,108 @@
+import type { Context, ServiceSettingSchema, ServiceSchema } from "moleculer";
+import { Service as DbService } from "@moleculer/database";
+import type { DatabaseMethods } from "@moleculer/database";
+
+export type DbServiceMethods = DatabaseMethods & {
+	seedDB?(): Promise<void>;
+};
+
+type DbServiceSchema = Partial<ServiceSchema<ServiceSettingSchema, DbServiceMethods>>;
+
+export default function (collection: string): DbServiceSchema {
+	const cacheCleanEventName = `cache.clean.${collection}`;
+
+	const schema: DbServiceSchema = {
+		/**
+		 * Mixins. More info: https://moleculer.services/docs/0.15/services.html#Mixins
+		 */
+		mixins: [
+			// @moleculer/database config: More info: https://github.com/moleculerjs/database
+			DbService({
+				adapter:
+					// In production use MongoDB
+					process.env.DB_URI?.startsWith("mongodb://")
+						? {
+								type: "MongoDB",
+								options: {
+									uri: process.env.DB_URI
+								}
+							}
+						: {
+								type: "NeDB",
+								options:
+									// In unit/integration tests use in-memory DB. Jest sets the NODE_ENV automatically
+									// During dev use file storage
+									process.env.NODE_ENV === "test"
+										? {
+												neDB: {
+													inMemoryOnly: true
+												}
+											}
+										: `./data/${collection}.db`
+							},
+				strict: false
+			})
+		],
+
+		/**
+		 * Events. More info: https://moleculer.services/docs/0.15/events.html
+		 */
+		events: {
+			/**
+			 * Subscribe to the cache clean event. If it's triggered
+			 * clean the cache entries for this service.
+			 */
+			async [cacheCleanEventName]() {
+				if (this.broker.cacher) {
+					await this.broker.cacher.clean(`${this.fullName}.*`);
+				}
+			}
+		},
+
+		/**
+		 * Methods. More info: https://moleculer.services/docs/0.15/services.html#Methods
+		 */
+		methods: {
+			/**
+			 * Send a cache clearing event when an entity changed.
+			 *
+			 * @param {String} type
+			 * @param {object} data
+			 * @param {object} oldData
+			 * @param {Context} ctx
+			 * @param {object} opts
+			 */
+			async entityChanged(
+				type: string,
+				data: object,
+				oldData: object,
+				ctx: Context,
+				opts: object
+			) {
+				ctx.broadcast(cacheCleanEventName);
+			}
+		},
+
+		/**
+		 * Service started lifecycle event handler
+		 * More info: https://moleculer.services/docs/0.15/lifecycle.html#started-event-handler
+		 */
+		async started() {
+			// Check the count of items in the DB. If it's empty,
+			// call the `seedDB` method of the service.
+			if (this.seedDB) {
+				const adapter = await this.getAdapter();
+				const count = await adapter.count();
+				if (count == 0) {
+					this.logger.info(
+						`The '${collection}' collection is empty. Seeding the collection...`
+					);
+					await this.seedDB();
+					this.logger.info("Seeding is done. Number of records:", await adapter.count());
+				}
+			}
+		}
+	};
+
+	return schema;
+};
---
+++ test/unit/mixins/db.mixin.spec.ts
@@ -0,0 +1,99 @@
+import { afterAll, beforeAll, describe, it, expect, vi } from "vitest";
+
+import { ServiceBroker } from "moleculer";
+import DbMixin from "../../../mixins/db.mixin.js";
+
+describe("Test DB mixin", () => {
+	describe("Test schema generator", () => {
+		const broker = new ServiceBroker({ logger: false, cacher: "Memory" });
+
+		beforeAll(() => broker.start());
+		afterAll(() => broker.stop());
+
+		it("check schema properties", async () => {
+			const schema = DbMixin("my-collection") as any;
+
+			expect(schema.started).toBeDefined();
+			expect(schema.events["cache.clean.my-collection"]).toBeInstanceOf(Function);
+			expect(schema.methods.entityChanged).toBeInstanceOf(Function);
+		});
+
+		it("check cache event handler", async () => {
+			vi.spyOn(broker.cacher!, "clean");
+
+			const schema = DbMixin("my-collection") as any;
+
+			await schema.events["cache.clean.my-collection"].call({
+				broker,
+				fullName: "my-service"
+			});
+
+			expect(broker.cacher!.clean).toBeCalledTimes(1);
+			expect(broker.cacher!.clean).toBeCalledWith("my-service.*");
+		});
+
+		describe("Check service started handler", () => {
+			it("should not call seedDB method", async () => {
+				const schema = DbMixin("my-collection") as any;
+
+				const adapterMock = {
+					count: vi.fn(async () => 10)
+				};
+				let getAdapterMock = vi.fn(() => {
+					return Promise.resolve(adapterMock);
+				});
+				const seedDBFn = vi.fn();
+
+				await schema.started.call({
+					broker,
+					logger: broker.logger,
+					getAdapter: getAdapterMock,
+					seedDB: seedDBFn
+				});
+
+				expect(adapterMock.count).toBeCalledTimes(1);
+				expect(adapterMock.count).toBeCalledWith();
+
+				expect(seedDBFn).toBeCalledTimes(0);
+			});
+
+			it("should call seedDB method", async () => {
+				const schema = DbMixin("my-collection") as any;
+
+				const adapterMock = {
+					count: vi.fn(async () => 0)
+				};
+				let getAdapterMock = vi.fn(() => {
+					return Promise.resolve(adapterMock);
+				});
+				const seedDBFn = vi.fn();
+
+				await schema.started!.call({
+					broker,
+					logger: broker.logger,
+					getAdapter: getAdapterMock,
+					seedDB: seedDBFn
+				});
+
+				expect(adapterMock.count).toBeCalledTimes(2);
+				expect(adapterMock.count).toBeCalledWith();
+
+				expect(seedDBFn).toBeCalledTimes(1);
+				expect(seedDBFn).toBeCalledWith();
+			});
+		});
+
+		it("should broadcast a cache clear event", async () => {
+			const schema = DbMixin("my-collection");
+
+			const ctx = {
+				broadcast: vi.fn()
+			};
+
+			await schema.methods!.entityChanged(null, null, null, ctx);
+
+			expect(ctx.broadcast).toBeCalledTimes(1);
+			expect(ctx.broadcast).toBeCalledWith("cache.clean.my-collection");
+		});
+	});
+});
